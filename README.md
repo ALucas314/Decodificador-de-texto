b43fa318a570f90f42a7a357acdea06a888c6652
Antônio Lucas C Araújo <156518068+ALucas314@users.noreply.github.com>
2024-07-18 11:23:06 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -2,4 +2,49 @@
 
 <img width="213" alt="Oracle+Alura" src="https://github.com/user-attachments/assets/923f2cf8-bb31-4a8a-89c7-4be6b7adbc96">
 
+## Índice
 
+-   [Descrição do Projeto](#descrição-do-projeto)
+-   [Funcionalidades e Demonstração da Aplicação](#funcionalidades-e-demonstração-da-aplicação)
+-   [Acesso ao Projeto](#acesso-ao-projeto)
+-   [Tecnologias e técnicas utilizadas](#tecnologias-e-técnicas-utilizadas)
+-   [Desenvolvedor do Projeto](#desenvolvedor-do-Projeto)
+
+## Descrição do Projeto
+
+Projeto desenvolvido como parte do programa de formação e empregabilidade [ONE](https://www.oracle.com/br/education/oracle-next-education/) desenvolvido pela [Oracle](https://www.oracle.com/br/) e ministrado pela [Alura](https://www.alura.com.br/).
+
+Trata-se de um aplicativo web responsivo e acessível que codifica e decodifica textos inseridos pelo usuário, exibindo na tela como resultado, respectivamente, as mensagens codificadas e descodificadas.
+
+## Funcionalidades e Demonstração da Aplicação
+
+Inicialmente há um campo onde deve-se inserir a mensagem a ser criptografada ou descriptografada. A mensagem a ser inserida (criptografada ou não) tem como requisitos não estar vazia e nem conter letras maiúsculas ou nem caracteres acentuados, no entanto a pontuação comum, como a vírgula e pontos final, de exclamação e de interrogação, podem se utilizados. O "c" cedilha (ç) também é aceito.
+
+No caso da não obediência dos requisitos supracitados, uma mensagem de alerta é exibida e a criptografia (ou descriptografia) não é efetuada.
+
+Obedecidos os requisitos e após o clique da operação desejada, a mensagem resultado é apresentada em campo próprio, juntamente com um botão "copiar".
+
+O botão "copia" tem a mesma função do "Ctrl + C", e se aplica apena à mensagem resultado.
+
+Abaixo é apresentado um curto vídeo demonstrando as funcionalidades do aplicativo:
+
+https://github.com/user-attachments/assets/34fdc7ef-4e4f-44d4-be9d-1af28472f1d2
+
+## Acesso ao Projeto
+
+-   [Repositório no GitHub](https://github.com/Choconaldo/Decodificador-de-Texto-Challenge-Alura-Oracle-ONE)
+-   [Deploy do aplicativo](https://choconaldo.github.io/Decodificador-de-Texto-Challenge-Alura-Oracle-ONE/)
+
+## Tecnologias e técnicas utilizadas
+
+-   `HTML5`
+-   `CSS3`
+-   `JavaScript`
+-   `Responsividade`
+-   `Acessibilidade`
+-   `Mobile first`
+
+## Desenvolvedor do Projeto
+
+| [<img loading="lazy" src="https://avatars.githubusercontent.com/u/148563205?v=4" width=150><br><sub>Leonardo Tresso Mendes</sub>](https://github.com/Choconaldo) |
+| :--------------------------------------------------------------------------------------------------------------------------------------------------------------: |
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Captura dos elementos do DOM
-
 const criptoTextEl = document.getElementById("cripto");
 const decriptoTextEl = document.getElementById("decripto");
 const criptoBtnEl = document.getElementById("criptoBtn");
@@ -10,128 +9,132 @@ const decriptoImgEl = document.getElementById("decripto-img");
 const decriptoBoxEl = document.querySelector(".decripto-box");
 
 // Adição dos "Event Listeners" nos botões
-
 criptoBtnEl.addEventListener("click", encriptText);
-
 decriptoBtnEl.addEventListener("click", decriptText);
-
 copyBtnEl.addEventListener("click", copyText);
 
 // Função de encriptação
-
 function encriptText() {
-	const regularText = criptoTextEl.textContent;
-	const anyInvalidChar = isAnyCharInvalid(regularText);
-	if (anyInvalidChar) {
-		renderInvalid(regularText);
-	} else {
-		let encriptedText = "";
-		for (letter of regularText) {
-			switch (letter) {
-				case "e":
-					encriptedText += "enter";
-					break;
-				case "i":
-					encriptedText += "imes";
-					break;
-				case "a":
-					encriptedText += "ai";
-					break;
-				case "o":
-					encriptedText += "ober";
-					break;
-				case "u":
-					encriptedText += "ufat";
-					break;
-				default:
-					encriptedText += letter;
-			}
-		}
-		renderValid(encriptedText);
-	}
+  const regularText = criptoTextEl.textContent;
+  const anyInvalidChar = isAnyCharInvalid(regularText);
+  if (anyInvalidChar) {
+    renderInvalid(regularText);
+  } else {
+    let encriptedText = "";
+    for (letter of regularText) {
+      switch (letter) {
+        case "e":
+          encriptedText += "enter";
+          break;
+        case "i":
+          encriptedText += "imes";
+          break;
+        case "a":
+          encriptedText += "ai";
+          break;
+        case "o":
+          encriptedText += "ober";
+          break;
+        case "u":
+          encriptedText += "ufat";
+          break;
+        default:
+          encriptedText += letter;
+      }
+    }
+    renderValid(encriptedText, false); // Passa false para indicar que é criptografado
+  }
 }
 
 // Função de decriptação
-
 function decriptText() {
-	const encriptedText = criptoTextEl.textContent;
-	const anyInvalidChar = isAnyCharInvalid(encriptedText);
-	if (anyInvalidChar) {
-		renderInvalid(encriptedText);
-	} else {
-		let decriptedText = encriptedText
-			.replaceAll("enter", "e")
-			.replaceAll("imes", "i")
-			.replaceAll("ai", "a")
-			.replaceAll("ober", "o")
-			.replaceAll("ufat", "u");
-		renderValid(decriptedText);
-	}
+  const encriptedText = criptoTextEl.textContent;
+  const anyInvalidChar = isAnyCharInvalid(encriptedText);
+  if (anyInvalidChar) {
+    renderInvalid(encriptedText);
+  } else {
+    let decriptedText = encriptedText
+      .replaceAll("enter", "e")
+      .replaceAll("imes", "i")
+      .replaceAll("ai", "a")
+      .replaceAll("ober", "o")
+      .replaceAll("ufat", "u");
+    renderValid(decriptedText, true); // Passa true para indicar que é descriptografado
+    changeImage(); // Chama a função para mudar a imagem
+  }
 }
 
 // Função de validação da mensagem inserida pelo usuário
-
 function isAnyCharInvalid(text) {
-	if (text) {
-		let invalidChar = 0;
-		for (letter of text) {
-			if (
-				(letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) ||
-				letter.charCodeAt(0) == 32 ||
-				letter.charCodeAt(0) == 33 ||
-				letter.charCodeAt(0) == 44 ||
-				letter.charCodeAt(0) == 46 ||
-				letter.charCodeAt(0) == 63 ||
-				letter.charCodeAt(0) == 231
-			) {
-				invalidChar += 0;
-			} else {
-				invalidChar += 1;
-			}
-		}
-		return Boolean(invalidChar);
-	} else {
-		return true;
-	}
+  if (text) {
+    let invalidChar = 0;
+    for (letter of text) {
+      if (
+        (letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) ||
+        letter.charCodeAt(0) == 32 ||
+        letter.charCodeAt(0) == 33 ||
+        letter.charCodeAt(0) == 44 ||
+        letter.charCodeAt(0) == 46 ||
+        letter.charCodeAt(0) == 63 ||
+        letter.charCodeAt(0) == 231
+      ) {
+        invalidChar += 0;
+      } else {
+        invalidChar += 1;
+      }
+    }
+    return Boolean(invalidChar);
+  } else {
+    return true;
+  }
 }
 
 // Função que renderiza a mensagem resultado validada
+function renderValid(text, isDecrypted) {
+  decriptoTextEl.style.color = "black";
+  decriptoTextEl.style.fontWeight = 400;
+  decriptoTextEl.style.lineHeight = 1.5;
+  decriptoTextEl.style.textAlign = "left";
+  criptoTextEl.textContent = "";
+  decriptoSubtextEl.classList.add("invisible");
+  copyBtnEl.classList.remove("invisible");
+  decriptoImgEl.classList.remove("invisible");
+  decriptoImgEl.classList.remove("img-visibility");
+  decriptoBoxEl.style.justifyContent = "space-between";
+  decriptoTextEl.textContent = text;
 
-function renderValid(text) {
-	decriptoTextEl.style.color = "black";
-	decriptoTextEl.style.fontWeight = 400;
-	decriptoTextEl.style.lineHeight = 1.5;
-	decriptoTextEl.style.textAlign = "left";
-	criptoTextEl.textContent = "";
-	decriptoSubtextEl.classList.add("invisible");
-	copyBtnEl.classList.remove("invisible");
-	decriptoImgEl.classList.add("invisible");
-	decriptoImgEl.classList.remove("img-visibility");
-	decriptoBoxEl.style.justifyContent = "space-between";
-	decriptoTextEl.textContent = text;
+  if (isDecrypted) {
+    decriptoImgEl.src = "./img/Sasuke 1.png"; // Altera a imagem para Sasuke 1.png se for descriptografado
+  } else {
+    // Caso contrário, mantém a imagem padrão
+    decriptoImgEl.src = "./img/Sasuke.png";
+  }
 }
 
 // Função que renderiza a mensagem resultado invalidada
-
 function renderInvalid(text) {
-	decriptoTextEl.style.color = "red";
-	decriptoTextEl.style.fontWeight = 700;
-	decriptoTextEl.style.lineHeight = 1.2;
-	decriptoTextEl.style.textAlign = "center";
-	decriptoTextEl.textContent =
-		"Por favor, use apenas letras minúsculas e sem acento";
-	decriptoSubtextEl.classList.remove("invisible");
-	copyBtnEl.classList.add("invisible");
-	decriptoImgEl.classList.remove("invisible");
-	decriptoImgEl.classList.add("img-visibility");
-	decriptoBoxEl.style.justifyContent = "center";
-	criptoTextEl.textContent = text;
+  decriptoTextEl.style.color = "red";
+  decriptoTextEl.style.fontWeight = 700;
+  decriptoTextEl.style.lineHeight = 1.2;
+  decriptoTextEl.style.textAlign = "center";
+  decriptoTextEl.textContent =
+    "Por favor, use apenas letras minúsculas e sem acento";
+  decriptoSubtextEl.classList.remove("invisible");
+  copyBtnEl.classList.add("invisible");
+  decriptoImgEl.classList.remove("invisible");
+  decriptoImgEl.classList.add("img-visibility");
+  decriptoBoxEl.style.justifyContent = "center";
+  criptoTextEl.textContent = text;
 }
 
 // Função do botão "Copiar"
-
 function copyText() {
-	const userText = decriptoTextEl.textContent;
-	navigator.clipboard.writeText(userText);
-	decriptoTextEl.textContent = "Nenhuma mensagem";
+  const userText = decriptoTextEl.textContent;
+  navigator.clipboard.writeText(userText);
+  decriptoTextEl.textContent = "Nenhuma mensagem";
+}
+
+// Função para mudar a imagem ao descriptografar
+function changeImage() {
+  decriptoImgEl.src = "./img/Sasuke 1.png";
 }
